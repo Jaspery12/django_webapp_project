40e0e5a7ee438c9af89b28b1de71c225605b92b2
Jasper Yang <2889647y@student.gla.ac.uk>
2025-02-01 20:38:04 +0000

    5.8 unfinished

--- populate_rango.py
+++ populate_rango.py
@@ -28,9 +28,9 @@ def populate():
     {'title':'Flask',
       'url':'http://flask.pocoo.org'} ]
   
-  cats = {'Python': {'pages': python_pages},
-          'Django': {'pages': django_pages},
-          'Other Frameworks': {'pages': other_pages} }
+  cats = {'Python': {'pages': python_pages, 'Likes': Category.likes, 'Views': Category.views},
+          'Django': {'pages': django_pages, 'Likes': Category.likes, 'Views': Category.views},
+          'Other Frameworks': {'pages': other_pages, 'Likes': Category.likes, } }
 
   for cat, cat_data in cats.items():
     c = add_cat(cat)
@@ -48,8 +48,10 @@ def add_page (cat, title, url, views=0):
   p.save()
   return p 
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
   c = Category.objects.get_or_create(name=name)[0]
+  c.views = views 
+  c.likes = likes 
   c.save()
   return c 
 
--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
   name = models.CharField(max_length=128, unique=True)
+  views = models.IntegerField(default=0)
+  likes = models.IntegerField(default=0)
   
   class Meta:
     verbose_name_plural = 'Categories'
@@ -17,3 +19,6 @@ class Page(models.Model):
   
   def __str__(self):
     return self.title
+
+class PageAdmin(models.Model):
+  list_display = ('title', 'category', 'url')
