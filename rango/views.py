00d2a3de3aeeac853f2bec13e8ecd814aecd5330
Jasper Yang <2889647y@student.gla.ac.uk>
2025-02-11 15:44:17 +0000

    9.8-10

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page 
 from rango.forms import CategoryForm, PageForm, UserProfileForm, UserForm
-from django.contrib.auth import authenticate, login 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def index(request):
   category_list = Category.objects.order_by('-likes')[:5]
@@ -128,3 +129,12 @@ def user_login(request):
     
   else:
     return render(request, 'rango/login.html')
+  
+@login_required 
+def restricted(request):
+  return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+  logout(request)
+  return redirect(reverse('rango:index'))
